3132b0636d33e35c27bd399ff1e74541ef665438
williamJia <william_jia00@163.com>
2020-07-30 12:39:10 +0800

    协同过滤进一步封装，同时兼容基于 user and item

--- main/app/rec/shop_rec_cf.py
+++ main/app/rec/shop_rec_cf.py
@@ -4,10 +4,23 @@ sys.path += ['/data/data_big/jyp/shopping_reommendation_py']
 from main.app.dataProcess.shopping_ui_score import shopping_data
 from main.app.util.model.cf_knn_basic import cf_knn_basic
 
-class shop_rec_cf:
+class shop_rec:
 
-    def shop_rec_cf_item_base(self,ret_path='shop_rec_cf.csv',topN=50,model_path='item_base.index'):
-        ''' 基于 itemBase 的协同过滤算法 '''
+    def __init__(self,ret_path='shop_rec_cf_item.csv',topN=50,model_path='model.index',user_based=False):
+        '''
+        多路召回算法
+        :param ret_path: 返回结果储存路径
+        :param topN: 相似 topN 的数量
+        :param model_path: 模型地址
+        :param user_based: 协同过滤算法中，是否以 userBase 来计算，默认否，基于itemBase计算
+        '''
+        self.ret_path = ret_path
+        self.topN = topN
+        self.model_path = model_path
+        self.user_based = user_based
+
+    def shop_rec_cf(self):
+        ''' 协同过滤算法 '''
         # 获取数据
         sd = shopping_data()
         df = sd.get_shopping_ui_score()
@@ -15,15 +28,17 @@ class shop_rec_cf:
         print("get data over")
 
         # 模型训练&预测结果
-        ckb = cf_knn_basic()
-        print("model train begin")
-        ret = ckb.fit_transform(df,topN,model_path=model_path,sub_num=10240)
+        ckb = cf_knn_basic(user_based=self.user_based)
+        ret = ckb.fit_transform(df,self.topN,model_path=self.model_path,sub_num=10240)
         ret_df = ret.groupby('id',as_index=False).first()
-        print("model over")
-        ret_df.to_csv(ret_path,index=False,chunksize=10240)
+        ret_df.to_csv(self.ret_path,index=False,chunksize=10240)
 
         print("save data & shop_rec_cf_item_base over")
 
 if __name__ == '__main__':
-    sr = shop_rec_cf()
-    sr.shop_rec_cf_item_base()
+    # 模型初始化
+    sr = shop_rec(ret_path='rec_shop_cf_item.csv',model_path='item_base.index',user_based=False) # 基于 item 的协同过滤
+    # sr = shop_rec(ret_path='rec_shop_cf_user.csv',model_path='user_base.index',user_based=True)  # 基于 user 的协同过滤 --》占用内存巨大，pass
+    sr.shop_rec_cf()
+
+
